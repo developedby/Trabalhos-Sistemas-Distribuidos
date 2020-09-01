99aa908d81a112e0d9bba29a435f8a331ed82083
Álefe Felipe Gonçalves Pereira Dias <alefe@utfpr.edu.br>
2020-09-01 13:58:28 -0300

    Começa trabalho 2

---
+++ trabalho 2/server.py
@@ -0,0 +1,13 @@
+import requests
+
+#OHLC data
+r = requests.get('https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1572651390&to=1572910590')
+print(r.text)
+
+#Tick data
+r = requests.get('https://finnhub.io/api/v1/stock/tick?symbol=AAPL&date=2020-03-25')
+print(r.text)
+
+# Quote data
+r = requests.get('https://finnhub.io/api/v1/quote?symbol=AAPL')
+print(r.text)
