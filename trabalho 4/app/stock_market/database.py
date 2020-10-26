f70d1b32dbf175903e890ca0c0b2bd38821e7b09
Nicolas Abril <nicolas.abril@gmail.com>
2020-10-25 23:18:00 -0300

    Add get_stock_owned_by_client no wrapper do db

--- trabalho 4/app/stock_market/database.py
+++ trabalho 4/app/stock_market/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 import threading
+from typing import Dict
 
 from ..order import Order, OrderType
 
@@ -22,3 +23,12 @@ class Database:
         data = self.execute(
             f'''select * from {order_type.value} where id = {order_id}''').fetchone()
         return Order(data[1], order_type, data[2], data[3], data[4], data[5], data[6])
+
+    def get_stock_owned_by_client(self, client_name: str) -> Dict[str, float]:
+        """Retorna a carteira de ações de um cliente."""
+        command = f"""
+            select ticker, amount from OwnedStock
+            where client_id = (select id from Client where name = '{client_name}')
+        """
+        data = self.execute(command)
+        return {entry[0]: entry[1] for entry in data}
